7a33e81472522c7ef022ac6015facfed3a4ed9a7
Expired <imoses2@hotmail.com>
2022-03-14 21:49:42 -0700

    fixes #10

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -29,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // use handlebars
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
